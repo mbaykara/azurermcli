b40603ed5d59748c2c923a07be9e5cbd3fb45683
Mehmet Ali Baykara <mab.baykara@gmail.com>
2024-12-22 20:33:41 +0100

    cleanup

--- cmd/azr/main.go
+++ cmd/azr/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/mbaykara/azurermcli/internal/app"    // Replace 'yourusername' with your GitHub username
-    "github.com/mbaykara/azurermcli/internal/azure"  // Replace 'yourusername' with your GitHub username
-    tea "github.com/charmbracelet/bubbletea"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/mbaykara/azurermcli/internal/app"
+	"github.com/mbaykara/azurermcli/internal/azure"
 )
 
 func main() {
-    p := tea.NewProgram(app.New(), tea.WithAltScreen())
-    
-    // Start by fetching subscriptions
-    go func() {
-        msg := azure.FetchSubscriptions()
-        p.Send(msg)
-    }()
-    
-    if _, err := p.Run(); err != nil {
-        fmt.Printf("Error: %v", err)
-        os.Exit(1)
-    }
-} 
+	p := tea.NewProgram(app.New(), tea.WithAltScreen())
+
+	// Start by fetching subscriptions
+	go func() {
+		msg := azure.FetchSubscriptions()
+		p.Send(msg)
+	}()
+
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
